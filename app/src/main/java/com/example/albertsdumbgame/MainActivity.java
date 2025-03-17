83a78c01e79f00a1db3ff07729654571a62acb82
ng5134 <ng5134@bs.amalnet.k12.il>
2025-03-17 20:56:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/albertsdumbgame/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.albertsdumbgame;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvup1=findViewById(R.id.);
+
+    }
+
+    public void go1(View view) {
+    }
+
+    public void go2(View view) {
+    }
+
+    public void go3(View view) {
+    }
+
+    public void go4(View view) {
+    }
+}
